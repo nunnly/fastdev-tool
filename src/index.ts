4f49be2e875f0b327bc05cb4f52fa994b5045210
hawk <whktk@qq.com>
2020-04-02 23:34:15 +0800

    添加install方法支持vue快速安装

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import {config} from './config';
+import {Dict} from './Dict';
+import {rule, FormRule} from './FormRule';
+
 
 export {config} from './config';
 export {Fetch} from './decorator';
-
 export {Dict} from './Dict';
 export {rule, FormRule} from './FormRule';
+
+export interface FastDevToolOptions{
+  fetchErrorHandle: (e: Error) => void;
+}
+
+export function install (Vue:any, options:FastDevToolOptions){
+  Vue.prototype.$rule = rule;
+  Vue.prototype.$dict = new Dict();
+  if(options.fetchErrorHandle){
+    config.fetchErrorHandle = options.fetchErrorHandle;
+  }
+}
